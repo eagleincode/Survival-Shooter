fb852194410fbbf398b180941e5e539e61c52d8d
Mond Wan <mondwan.1015@gmail.com>
2015-01-25 18:30:42 +0800

    Part6: Player Health
    --Overview
    1. How enemy attack player
    2. Visual effect for player's dead
    3. Replace line ending from window style to unix style

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,20 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyAttack : MonoBehaviour
 {
+    // As named, attack speed of a bunny
     public float timeBetweenAttacks = 0.5f;
+
+    // Damage can deal of a bunny
     public int attackDamage = 10;
 
-
+    // A reference to the animator component
     Animator anim;
+
+    // A reference to the player GameObject
     GameObject player;
+
+    // A reference to the player's PlayerHealth component
+    // NOTE1: Class::PlayerHealth is defined on PlayerHealth.cs
     PlayerHealth playerHealth;
+
     //EnemyHealth enemyHealth;
+
+    // Whether player is close enough for bunny to attack
     bool playerInRange;
+
+    // Timer for couting up the next attack
+    // NOTE1: work with variable timeBetweenAttacks
     float timer;
 
-
+    // Setup references
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player");
@@ -23,20 +37,18 @@ public class EnemyAttack : MonoBehaviour
         anim = GetComponent <Animator> ();
     }
 
-
+    // Callback for bunny's sphere collider if it collides with others
     void OnTriggerEnter (Collider other)
     {
-        if(other.gameObject == player)
-        {
+        if (other.gameObject == player) {
             playerInRange = true;
         }
     }
 
-
+    // Callback for bunny's sphere collider if the collision ends
     void OnTriggerExit (Collider other)
     {
-        if(other.gameObject == player)
-        {
+        if (other.gameObject == player) {
             playerInRange = false;
         }
     }
@@ -46,13 +58,11 @@ public class EnemyAttack : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
-        {
+        if (timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/) {
             Attack ();
         }
 
-        if(playerHealth.currentHealth <= 0)
-        {
+        if (playerHealth.currentHealth <= 0) {
             anim.SetTrigger ("PlayerDead");
         }
     }
@@ -62,8 +72,7 @@ public class EnemyAttack : MonoBehaviour
     {
         timer = 0f;
 
-        if(playerHealth.currentHealth > 0)
-        {
+        if (playerHealth.currentHealth > 0) {
             playerHealth.TakeDamage (attackDamage);
         }
     }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,26 +1,48 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
+    // Initial health point
     public int startingHealth = 100;
+
+    // Current health point
     public int currentHealth;
+
+    // A reference to the slider view
     public Slider healthSlider;
+
+    // A reference to the damage view
     public Image damageImage;
+
+    // A clip will be played when player dies
     public AudioClip deathClip;
+
+    // Speed the damageImage will fade at
     public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
+    // Colour the damageImage is set to
+    public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
 
+    // A reference to the Animator component
     Animator anim;
+
+    // A reference to the AudioSource component
     AudioSource playerAudio;
+
+    // A reference to the player's movement which defined on PlayerMovement.cs
     PlayerMovement playerMovement;
+
     //PlayerShooting playerShooting;
+
+    // Whether the player is dead
     bool isDead;
+
+    // True when the player get damaged
     bool damaged;
 
-
+    // Setup reference for this instance
     void Awake ()
     {
         anim = GetComponent <Animator> ();
@@ -33,46 +55,60 @@ public class PlayerHealth : MonoBehaviour
 
     void Update ()
     {
-        if(damaged)
-        {
+        // Change damageImage colour
+        if (damaged) {
+            // Change to flashColour if player had been hitted
             damageImage.color = flashColour;
+        } else {
+            // Clear the flashColour in a fading manner
+            damageImage.color = Color.Lerp (
+                damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
-        else
-        {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
+        // Reset the damaged flag
         damaged = false;
     }
 
-
+    // Handler for receving damage from Enemy
     public void TakeDamage (int amount)
     {
+        // Set damaged flag so that Update() can change colour accordingly
         damaged = true;
 
+        // Deduct the health point by damage amount
         currentHealth -= amount;
 
+        // Update value in healthSlider to reflect the current health
         healthSlider.value = currentHealth;
 
+        // Play the hurt sound effect
         playerAudio.Play ();
 
-        if(currentHealth <= 0 && !isDead)
-        {
+        // Let player die if it's current health belows zero
+        // flag *isDead* ensures player dies one only
+        if (currentHealth <= 0 && !isDead) {
+            // Call Death() to handle what should do after player die
             Death ();
         }
     }
 
-
+    // Handler if player die
     void Death ()
     {
+        // Set flag *isDead* so that it won't be called twiced
         isDead = true;
 
         //playerShooting.DisableEffects ();
 
+        // Instruct the animator that player is dead
         anim.SetTrigger ("Die");
 
+        // Play the death clip
+        // NOTE1: reason for changing clip is because of replacing the hurt
+        //        sound effect
         playerAudio.clip = deathClip;
         playerAudio.Play ();
 
+        // Disable movment script
         playerMovement.enabled = false;
         //playerShooting.enabled = false;
     }
