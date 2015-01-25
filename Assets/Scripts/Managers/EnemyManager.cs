0212b9c082ab7a49f02ae8568d84e1829a3f5b5d
Mond Wan <mondwan.1015@gmail.com>
2015-01-25 22:12:10 +0800

    Part9: Spawn enemy
    --Overview
    1. Introduce how to use prefab
    2. How to override animation controller
    3. Spawn different type of enemy
    4. Multiple spawn agent

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,27 +2,40 @@
 
 public class EnemyManager : MonoBehaviour
 {
+    // A reference to the player health
     public PlayerHealth playerHealth;
+
+    // A reference to the enemy prefab
     public GameObject enemy;
+
+    // How long between each spawn
     public float spawnTime = 3f;
+
+    // Define where are spawn points
     public Transform[] spawnPoints;
 
-
+    // Game start
     void Start ()
     {
+        // Wait spawnTime first, then call Spawn() in number of spawnTime times
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
 
     void Spawn ()
     {
-        if(playerHealth.currentHealth <= 0f)
-        {
+        // Do nothing if player die
+        if (playerHealth.currentHealth <= 0f) {
             return;
         }
 
+        // Define where will be the spawn point
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 
-        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        // Instantiate a kind of enemy at spawnPoints
+        Instantiate (
+            enemy,
+            spawnPoints [spawnPointIndex].position,
+            spawnPoints [spawnPointIndex].rotation);
     }
 }
