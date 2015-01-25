089466924dcd213a75d6ed42a2809e7056db31f9
Mond Wan <mondwan.1015@gmail.com>
2015-01-25 15:18:31 +0800

    Part4: Create enemy
    --Overview
    1. Introduce nav agent for path finding
    2. Setup enemy animation
    3. Enemy will always head to player

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,19 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // Who will be traced by enemy
     Transform player;
     //PlayerHealth playerHealth;
     //EnemyHealth enemyHealth;
     NavMeshAgent nav;
 
-
+    // Setup references for this instance
     void Awake ()
     {
+        // Get reference of the player's transform
         player = GameObject.FindGameObjectWithTag ("Player").transform;
         //playerHealth = player.GetComponent <PlayerHealth> ();
         //enemyHealth = GetComponent <EnemyHealth> ();
+        // Get reference for nav
         nav = GetComponent <NavMeshAgent> ();
     }
 
@@ -22,7 +25,11 @@ public class EnemyMovement : MonoBehaviour
     {
         //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         //{
-            nav.SetDestination (player.position);
+        // Instrcut enemy heads to where player is
+        // 
+        // NOTE1: Nav ensure bunny won't crashed with obstacles like other
+        // bunnys or environment obstacles
+        nav.SetDestination (player.position);
         //}
         //else
         //{
