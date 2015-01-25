a1b2d8ea423155cfa3c1ba2f899194ade0573689
Mond Wan <mondwan.1015@gmail.com>
2015-01-25 23:05:23 +0800

    Part 10: Game Over

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -2,23 +2,35 @@
 
 public class GameOverManager : MonoBehaviour
 {
+    // A reference to player health
     public PlayerHealth playerHealth;
 
+    // How long we will restart the game after player die
+    public float restartDelay = 5f;
 
+    // A reference to animation
     Animator anim;
 
+    // A timer count up to restart the level
+    float restartTimer;
 
-    void Awake()
+    void Awake ()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator> ();
     }
 
 
-    void Update()
+    void Update ()
     {
-        if (playerHealth.currentHealth <= 0)
-        {
-            anim.SetTrigger("GameOver");
+        // Gameover if player health point is zero
+        if (playerHealth.currentHealth <= 0) {
+            anim.SetTrigger ("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay) {
+                Application.LoadLevel (Application.loadedLevel);
+            }
         }
     }
 }
